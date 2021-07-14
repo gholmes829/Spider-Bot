295ffff3be8567b1dfd524d6ef13f50a3bb90149
Sutton <c56332@txt.textron.com>
2021-07-14 11:44:30 -0500

    added eval method

--- driver.py
+++ driver.py
@@ -14,7 +14,7 @@ class Driver:
         self.env = SpiderBotSimulator(os.path.join(self.cwd, 'urdfs', 'spider_bot_v0.urdf'), real_time=False, gui=True)
     
     def run(self, args: list) -> None:
-        self.showcase()
+        self.evaluate()
         
     def showcase(self) -> None:
         i = 0
@@ -34,6 +34,22 @@ class Driver:
         
     def train(self):
         pass
+
+    def evaluate(self):
+        i = 0
+        j = 0
+        done = False
+        while j < 3:
+            controls = [-5 * np.sin(i / 10) for _ in range(4)]
+            observation, reward, done, info = self.env.step(controls)
+            pos, vel = observation['pos'], observation['vel']  # break down state of joints
+            # log pos and vel
+            i += 1
+            if done or i > 500:
+                self.env.reset()
+                j += 1
+                i = 0
+        self.env.close()
     
     def graph_data(self):
         pass

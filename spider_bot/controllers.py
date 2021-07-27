ecb937291f5e30f3fa9bd9ffff8462b7b1cdda53
Connor <connorsutton07@gmail.com>
2021-07-26 23:36:52 -0500

    made nn inherit from member class

--- spider_bot/controllers.py
+++ spider_bot/controllers.py
@@ -1,7 +1,8 @@
-from numba import jit
 import numpy as np
+from numba import jit
+from genetics import Member
 
-class FFNN:
+class FFNN (Member):
 	"""
 	Feed forward neural network without backpropogation.
 	Public Methods
@@ -9,13 +10,17 @@ class FFNN:
 	feedForward(inputs) -> np.ndarray:
 		Feeds inputs through neural net to obtain output.
 	"""
-	def __init__(self, layerSizes: list, activation: str = "sigmoid", weights: list = None, biases: list = None) -> None:
+	def __init__(self, layerSizes: list, id: int = None, activation: str = "sigmoid", weights: list = None, biases: list = None) -> None:
+        super(Member, self).__init__(id)
+
 		"""
 		Initializes.
 		Parameters
 		----------
 		layerSizes: list
 			Layer architecture
+        id: int
+            unique base-32 indentifier
 		activation: str, default="sigmoid"
 			String denoting activation function to use
 		weights: list, optional
@@ -23,6 +28,7 @@ class FFNN:
 		biases: list, optional
 			List of arrays of biases for each layer, randomized if not passed in
 		"""
+
 		activations = {
 			"sigmoid": FFNN.sigmoid,
 			"reLu": FFNN.reLu,
@@ -67,43 +73,3 @@ class FFNN:
 		e = np.exp(v)
 		return e / e.sum()
 
-def mutate(network: object) -> object:
-    """
-    Provides mutated clone of network.
-    Mutates by assigning random value to a given number of 
-    weights, biases, or activations
-
-    """
-    mutant = deepcopy(network)
-    target = choice(("weights", "biases"))
-
-    if target == "weights":
-        randLayer = randint(0, len(network["weights"]) - 1)
-        randNeuron = randint(0, len(network["weights"][randLayer]) - 1)
-        randWeight = randint(0, len(network["weights"][randLayer][randNeuron]) - 1)
-        network["weights"][randLayer][randNeuron][randWeight] = np.random.randn()
-    else:
-        randLayer = randint(0, len(network["biases"]) - 1)
-        randBias = randint(0, len(network["biases"][randLayer]) - 1)
-        network["biases"][randLayer][randBias] = np.random.randn()
-            
-    return mutant
-    
-def _crossover(self, parent1: object, parent2: object) -> object:
-    """
-    Cross over traits from parents for new member.
-    Decides to cross over parents' weights or biases. Selects a random neuron to crossover.
-
-    """
-    child1, child2 = deepcopy(parent1), deepcopy(parent2)
-    target = choice(("weights", "biases"))
-
-    randLayer = randint(0, len(child[target]) - 1)
-    randTargetElement = randint(0, len(child1[target][randLayer]) - 1)
-    temp = deepcopy(child1)
-    child1[target][randLayer][randTargetElement] = child2[target][randLayer][randTargetElement]
-    child2[target][randLayer][randTargetElement] = temp[target][randLayer][randTargetElement]
-
-    # fitness1, fitness2 = self.task(child1)["fitness"], self.task(child2)["fitness"]
-    best, parentId = (child1, parent2.id) if fitness1 > fitness2 else (child2, parent2.id)
-    self._updateMemberId(best)
